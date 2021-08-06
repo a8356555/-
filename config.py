670bc02604cd59d22fccfef62bee99a9fef04046
a8356555 <a8356555@gmail.com>
2021-08-06 14:06:27 +0000

    update utils for df functions

--- config.py
+++ config.py
@@ -7,33 +7,35 @@ from .utils import save_config, load_config, _handle_ckpt_path_and_model_version
 
 # data config
 class dcfg: 
-    input_path = '/content/gdrive/MyDrive/SideProject/YuShanCompetition/train'
+    input_path = Path('/content/gdrive/MyDrive/SideProject/YuShanCompetition/train')
     is_gpu_used = True # use GPU or not
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     is_memory_pinned= True
-    batch_size = 512 # batch size
+    batch_size = 128 # batch size
     num_workers = 4 # how many workers for loading data
     is_shuffle = True
+    transform_approach = 'replicate + gaussian noise'
+    class_num = 801
+    expected_num_per_class = 100
 
 #model config
 class mcfg: 
-    root_model_folder = '/content/gdrive/MyDrive/SideProject/YuShanCompetition/model'
+    root_model_folder = Path('/content/gdrive/MyDrive/SideProject/YuShanCompetition/model')
     today = str(date.today())
 
-    model_type = 'res18'       ### 請修改
-    description = 'train on gray scale and directly on original data with no second data source, use dali to speed up'         ### 請修改
-    is_continued = False         ### 請修改
-    is_new_version = False      ### 請修改
-    continued_folder_name = 'dali_test'     ### 請修改
-    ckpt_path, model_type, version = _handle_ckpt_path_and_model_version(is_continued, root_model_folder, model_type, is_new_version, today, continued_folder_name)
-    model_folder_path = os.path.join(root_model_folder, model_type, version)
+    model_type = 'effb0'       ### 請修改
+    description = 'train directly on original data with no second data source, using dali'### 請修改
+    is_continued = True         ### 請修改
+    is_new_continued_version = False      ### 請修改
+    new_continued_folder_name = 'continued_epoch_30'     ### 請修改
+    ckpt_path, model_type, version = _handle_ckpt_path_and_model_version(is_continued, root_model_folder, model_type, is_new_continued_version, today, new_continued_folder_name)
+    model_folder_path = root_model_folder / model_type / version
 
     is_pretrained = True        ### 請修改
     is_customized = False       ### 請修改
-    max_epochs = 80             ### 請修改
-    save_every_n_epoch = 10      ### 請修改    
+    max_epochs = 30           ### 請修改
+    save_every_n_epoch = 3      ### 請修改
 
-    pred_size = 801
     log_every_n_steps = 50         
     save_top_k_models = 2    
     gpus = 1
@@ -45,12 +47,19 @@ class mcfg:
 class ocfg:
     optim_name = 'Adam'         ### 請修改
     lr = 1e-3                   ### 請修改    
-    has_differ_lr = True         ### 請修改
+    has_differ_lr = False         ### 請修改
     lr_group = [lr/100, lr/10, lr] if has_differ_lr else lr     ### 請修改
     weight_decay = 0                                           ### 請修改
     momentum = 0.9 if optim == 'SGD' else 0                    ### 請修改
     
     has_scheduler = False
     schdlr_name = 'OneCycleLR' if has_scheduler else None
-    total_steps = (mcfg.max_epochs)*(len(train_image_paths)//dcfg.batch_size+1) if has_scheduler else None
+    total_steps = (mcfg.max_epochs)*(dcfg.class_num*dcfg.expected_num_per_class//dcfg.batch_size+1) if has_scheduler else None
     max_lr = [lr*10 for lr in lr_group] if has_scheduler else None
+
+
+# show some information
+config = load_config(mcfg.model_folder_path)
+print('\nModel Detail Check:\n - model ckpt path: ', mcfg.ckpt_path, '\n - model folder path: ', mcfg.model_folder_path, '\n - model description: ', mcfg.description)
+if config:
+    print(' - model description before: ', config['other_settings'])
--- utils.py
+++ utils.py
@@ -3,28 +3,9 @@ import cv2
 from PIL import Image
 import time
 import re
-import multiprocessing
 import os
-import shutil
-import tarfile
-import torch
-
-# load training data dict
-training_data_dict_path = '/content/gdrive/MyDrive/SideProject/YuShanCompetition/training data dic.txt'
-with open(training_data_dict_path, 'r') as file:
-    word_classes = [word.rstrip() for word in file.readlines()]
-print(f'no of origin labels: {len(word_classes)},\nno of unique labels: {np.unique(word_classes).shape[0]}')
-word_classes.append('isnull')
-
-def word2int_label(label):
-    """Transform word classes into integer labels (0~800)"""
-    word2int_label_dict = dict(zip(word_classes, range(len(word_classes))))
-    return word2int_label_dict[label]
-
-def int_label2word(int_label):
-    """Transform integer labels into word classes"""
-    int_label2word_dict = dict(zip(range(len(word_classes)), word_classes))
-    return int_label2word_dict[int_label]
+import numpy as np
+from pathlib import Path
 
 class ImageReader:
     @classmethod
@@ -34,24 +15,28 @@ class ImageReader:
 
     @classmethod
     def read_image_cv2(cls, path):
+        """Read and change RGB order"""
         img = cv2.imread(path)
         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         return img
 
     @classmethod
-    def show_image_and_label(cls, img_path, label, is_path_showed=False):
-        """
-        """
-        print(label, int_label2word(label),'\n')        
-        if is_path_showed:
-            print(img_path, '\n')
-            
-        image = cls.read_image_cv2(img_path)
-        plt.imshow(image)
+    def read_and_show_image(cls, path):
+        img = cv2.imread(path)
+        plt.imshow(img)
         plt.show()
+        return img
 
     @classmethod
-    def read_images(cls, file_paths, way='cv2'):
+    def show_image_and_label(cls, img_path, label, is_path_showed=False):
+        """"""
+        print(label,'\n')        
+        if is_path_showed:
+            print(img_path, '\n')            
+        cls.read_and_show_image(img_path)
+
+    @classmethod
+    def read_images_and_labels(cls, file_paths, way='cv2'):
         data = []
         if way == 'cv2':
             data = [[cls.read_image_cv2(path), re.search('[\u4e00-\u9fa5]{1}', path).group(0)] for path in file_paths]                                   
@@ -61,35 +46,56 @@ class ImageReader:
         return images, labels
 
     @classmethod
-    def read_images_mp(cls, file_paths):
-        """Using multi-processing to speed up reading images from paths
-        """
-
-        def worker(path, return_list, i):
-            """worker function"""
+    def _mp_for_images(cls, file_paths, target=None):
+        def get_worker(path, i, return_list):
             img = cv2.imread(path)
-            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-            return_list.append(img)
-            if (i+1) % 1000 == 0:
-                print(f'{i+1} pictures loaded')
+            if target == "shape":
+                return_list.append([img.shape, i])
+            elif target == "image":
+                img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+                return_list.append([img, i])            
+            del img
 
-        manager = multiprocessing.Manager()
-        return_list = manager.list()
+        def lazy_memory_worker(path, i, return_list):
+            cv2.imread(path)
+        import multiprocessing as mp
+        manager = mp.Manager()
         jobs = []
-        print(f'total images: {len(file_paths)}\nstart loading...')
+        length = len(file_paths)        
+        if target:
+            worker = get_worker 
+            return_list = manager.list()
+        else:
+            worker = lazy_memory_worker
+            return_list = None
+        print(f'total images: {length}\nstart loading...')
+        
         for i, path in enumerate(file_paths):
-            p = multiprocessing.Process(target=worker, args=(path, return_list, i))
+            p = mp.Process(target=worker, args=(path, i, return_list))
             jobs.append(p)
             p.start()
-
+            if (i+1) % 1000 == 0: 
+                print(f'{i} pics loaded')
+                for proc in jobs:
+                    proc.join()
+                jobs = []
         for proc in jobs:
-            proc.join()
-        return return_list
+            proc.join()                                
+        return [arr[0] for arr in return_list]
+
+    @classmethod
+    def lazy_read_image_mp(cls, file_paths):
+        cls._mp_for_images(file_paths)
+
+    @classmethod
+    def get_image_data_mp(cls, file_paths, target="shape"):
+        return cls._mp_for_images(file_paths, target)
  
 class FileHandler:
     @classmethod
     def tar_file(cls, file_url):
-        start = time.time()
+        import tarfile
+        start = time.time()        
         with tarfile.open(file_url) as file:
             file.extractall(os.path.dirname(file_url),)
             print(f'done! time: {time.time() - start}')
@@ -97,6 +103,7 @@ class FileHandler:
     @classmethod
     def copyfolder(cls, src, dst, symlinks=False, ignore=None):
         """Copy entire folder from src to dst"""
+        import shutil
         for item in os.listdir(src):
             s = os.path.join(src, item)
             d = os.path.join(dst, item)
@@ -106,12 +113,47 @@ class FileHandler:
                 shutil.copy2(s, d)
 
     @classmethod
-    def save_path_and_label_as_txt(cls, txt_path, paths):
+    def save_paths_and_labels_as_txt(cls, txt_path, paths, labels):
         with open(txt_path, 'w') as out_file:
-            for path in paths:
-                label = re.search("[\u4e00-\u9fa5]{1}", path).group(0)
-                label = word2int_label(label)
+            for path, label in zip(paths, labels):                
                 out_file.write(f'{path} {label}\n')
+
+    @classmethod
+    def make_train_valid_data_txt(cls):
+        """Call only once to make """
+        def custom_func(gp_df):
+            num = gp_df.shape[0]
+            gp_df = pd.concat([gp_df]*(100//num+1))
+            return gp_df
+
+        df_all, df_revised, df_checked = cls.load_target_dfs()
+        
+        not_null_cond = (df_revised['is_deleted'] == False) & (df_revised['label'] != 'isnull')
+        null_cond = df_revised['label'] == 'isnull'
+        df_valid_not_null = df_revised[not_null_cond].groupby('label').sample(12)
+        df_valid_null = df_revised[null_cond].sample(50)
+        df_valid = df_valid_not_null.append(df_valid_null, ignore_index=True)
+        
+        valid_image_paths, valid_int_labels = df_valid['path'].to_list(), df_valid['int_label'].to_list()
+        
+        df_train_not_null_raw = df_all[~df_all['path'].isin(df_checked['path'])]
+        df_train_not_null_revised = df_revised[(not_null_cond) & (~df_revised['path'].isin(valid_image_paths))]        
+        
+        df_train_not_null = df_train_not_null_raw.append(df_train_not_null_revised, ignore_index=True)
+        df_train_not_null = df_train_not_null.groupby('int_label').apply(custom_func).reset_index(drop=True).groupby('label').sample(100)
+        df_train_null = df_revised[(df_revised['label'] == 'isnull') & 
+                                      (~df_revised['path'].isin(df_valid_null['path']))]
+
+        df_train = df_train_not_null.append(df_train_null, ignore_index=True)
+        
+        train_image_paths, train_int_labels = df_train['path'].to_list(), df_train['int_label'].to_list()
+
+        valid_txt_path = '/content/gdrive/MyDrive/SideProject/YuShanCompetition/valid_balanced_images.txt'
+        cls.save_paths_and_labels_as_txt(valid_txt_path, valid_image_paths, valid_int_labels)
+
+        train_txt_path = '/content/gdrive/MyDrive/SideProject/YuShanCompetition/train_balanced_images.txt'
+        cls.save_paths_and_labels_as_txt(train_txt_path, train_image_paths, train_int_labels)
+
 
     @classmethod
     def read_path_and_label_from_txt(cls, txt_path):
@@ -121,18 +163,104 @@ class FileHandler:
         return paths, labels
 
 
+    @classmethod
+    def get_raw_df(cls):
+        root_paths = ['/content/gdrive/MyDrive/SideProject/YuShanTrain', '/content/gdrive/MyDrive/SideProject/YuShanCompetition/train']
+        image_paths = []
+        labels = []
+        for root_path in root_paths:
+            for root, _, files in os.walk(root_path):
+                for file in files:
+                    re_result = re.search("[\u4e00-\u9fa5]{1}", file)
+                    if re_result and 'modified' not in file:
+                        labels.append(re_result.group(0))
+                        image_paths.append(os.path.join(root, file)) 
+            
+        df_all = pd.DataFrame(list(zip(image_paths, labels)), columns = ['path', 'label'])
+        return df_all
+
+    @classmethod
+    def get_word_classes_dict(cls, path=None):        
+        training_data_dict_path = path if path else "/content/gdrive/MyDrive/SideProject/YuShanCompetition/training data dic.txt"
+        assert os.path.exists(training_data_dict_path), 'file does not exists or google drive is not connected'
+
+        with open(training_data_dict_path, 'r') as file:
+            word_classes = [word.rstrip() for word in file.readlines()]
+        print(f'no of origin labels: {len(word_classes)},\nno of unique labels: {np.unique(word_classes).shape[0]}')
+        word_classes.append('isnull')
+        return word_classes
+
+    @classmethod
+    def load_target_dfs(cls, df_all_path=None, df_revised_path=None, df_checked_path=None):
+        df_all_path = df_all_path or '/content/gdrive/MyDrive/SideProject/YuShanCompetition/all_data.csv'
+        df_revised_path = df_revised_path or '/content/gdrive/MyDrive/SideProject/YuShanCompetition/df_revised.csv'
+        df_checked_path = df_checked_path or '/content/gdrive/MyDrive/SideProject/YuShanCompetition/df_checked.csv'
+        df_all = pd.read_csv(df_all_path)
+        df_revised = pd.read_csv(df_revised_path)
+        df_checked = pd.read_csv(df_checked_path)
+        return df_all, df_revised, df_checked
+
+    @classmethod
+    def get_paths_and_int_labels(cls, train_txt_path=None, valid_txt_path=None):
+        train_txt_path = train_txt_path or '/content/gdrive/MyDrive/SideProject/YuShanCompetition/train_balanced_images.txt'
+        valid_txt_path = valid_txt_path or '/content/gdrive/MyDrive/SideProject/YuShanCompetition/valid_balanced_images.txt'
+        train_image_paths, train_int_labels = cls.read_path_and_label_from_txt(train_txt_path)
+        valid_image_paths, valid_int_labels = cls.read_path_and_label_from_txt(valid_txt_path)
+        return train_image_paths, train_int_labels, valid_image_paths, valid_int_labels
+
+    @classmethod
+    def make_noisy_student_training_data():
+        valid_txt_path = '/content/gdrive/MyDrive/SideProject/YuShanCompetition/valid_balanced_images.txt'
+        valid_image_paths, valid_int_labels = cls.read_path_and_label_from_txt(valid_txt_path)
+        df_all, df_revised, df_checked = load_target_dfs()
+    
+        not_null_cond = (df_revised['is_deleted'] == False) & (df_revised['label'] != 'isnull')
+        null_cond = df_revised['label'] == 'isnull'
+        df_clean = df_revised[(~df_revised['path'].isin(valid_image_paths)) & not_null_cond].groupby('label').sample(60)
+        clean_image_paths, clean_int_labels = df_clean
+
+
+word_classes = FileHandler.get_word_classes_dict()
+
+# data functions
+def word2int_label(label):
+    """Transform word classes into integer labels (0~800)"""
+    word2int_label_dict = dict(zip(word_classes, range(len(word_classes))))
+    return word2int_label_dict[label]
+
+def int_label2word(int_label):
+    """Transform integer labels into word classes"""
+    if isinstance(int_label, str):
+        int_label = int(int_label)
+    int_label2word_dict = dict(zip(range(len(word_classes)), word_classes))
+    return int_label2word_dict[int_label]
+
+
+
+
+# config function
+"""
+basic func and list model
+"""
+def _handle_not_exist_folder(folder_path):
+    is_existing = folder_path.exists()
+    print(f'test if {folder_path} exists: {is_existing}, if False then mkdir')
+    if not is_existing:
+        folder_path.mkdir(parents=True)
+        print(f'test again if {folder_path} exists: {folder_path.exists()}')
+
 def _get_ckpt_path(folder_path):
-    ckpt_dir_path = os.path.join(folder_path, 'checkpoints')
-    assert os.path.exists(ckpt_dir_path), 'there\'s still no ckeckpoint folder, please select another model version '
-    print('existing ckpt file: ', [file for file in os.listdir(ckpt_dir_path) if file.endswith('ckpt')])
+    ckpt_dir_path = folder_path / 'checkpoints'
+    assert ckpt_dir_path.exists(), 'there\'s still no ckeckpoint folder, please select another model version '
+    print('existing ckpt file: ', [x.name for x in ckpt_dir_path.glob("*.ckpt")])
     epoch_num = input('which one to use? enter the entire name: ')
-    ckpt_path = os.path.join(ckpt_dir_path, epoch_num)
-    while not os.path.exists(ckpt_path):
+    ckpt_path = ckpt_dir_path / epoch_num
+    while not ckpt_path.exists():
         epoch_num = input('the file dosen\'t exist, please enter again: ')
-        ckpt_path = os.path.join(ckpt_dir_path, epoch_num)
+        ckpt_path = ckpt_dir_path / epoch_num
     return ckpt_path
 
-def _handle_ckpt_path_and_model_version(is_continued, root_model_folder, model_type, is_new_version, today, continued_folder_name):
+def _handle_ckpt_path_and_model_version(is_continued, root_model_folder, model_type, is_new_continued_version, today, new_continued_folder_name):
     """
     3 phase
         1. enter model_type
@@ -140,40 +268,37 @@ def _handle_ckpt_path_and_model_version(is_continued, root_model_folder, model_t
         3. enter model checkpoint
     """
     if is_continued:        
-        target_model_type_folder = os.path.join(root_model_folder, model_type)        
-        while not os.path.exists(target_model_type_folder):            
-            print('existing model type: ', [m for m in os.listdir(root_model_folder) if not m.startswith('.')])
+        target_model_type_folder = root_model_folder / model_type
+        while not target_model_type_folder.exists():
+            print('existing model type: ', [x.name for x in root_model_folder.glob("*")])
             print('invalid input!')
             model_type = input('please input correct existing model type: (list above) ')
-            target_model_type_folder = os.path.join(root_model_folder, model_type)
+            target_model_type_folder = root_model_folder / model_type
         
-        print('existing model version', [v for v in os.listdir(target_model_type_folder) if not v.startswith('.')])
+        print('existing model version', [x.name for x in target_model_type_folder.glob("*")])
         version = input('please enter model version: ')         
-        target_model_folder = os.path.join(target_model_type_folder, version)
-        while not os.path.exists(target_model_folder):
+        target_model_folder = target_model_type_folder / version
+        while not target_model_folder.exists():
             print('invalid input!')
             version = input('please enter correct model version: ')
-            target_model_folder = os.path.join(target_model_type_folder, version)
+            target_model_folder = target_model_type_folder / version
 
-        version = (version + continued_folder_name) if is_new_version else version
+        version = (version + new_continued_folder_name) if is_new_continued_version else version
         ckpt_path = _get_ckpt_path(target_model_folder)
     else:
-        folder = os.path.join(root_model_folder, model_type)
-        if not os.path.exists(folder): os.mkdir(folder)
-        existing_ver = os.listdir(folder)
-        print('existing version: ', [re.search('v[0-9]+', v).group(0) for v in existing_ver if not v.startswith('.')])
+        folder = root_model_folder / model_type
+        _handle_not_exist_folder(folder)
+        print('existing version: ', [x.name for x in folder.glob("v[0-9]*")])
         version_num = input('please enter lastest version number(eg. v1): ') 
         version = f'{today}.{version_num}'
         ckpt_path = None
+        _handle_not_exist_folder(folder / version)
+
     return ckpt_path, model_type, version
 
 
-def save_config(folder_path='/content/', model=None):
-    parent_folder = os.path.abspath(os.path.dirname(folder_path))
-    if not os.path.exists(parent_folder):
-        os.mkdir(parent_folder)
-    if not os.path.exists(folder_path):
-        os.mkdir(folder_path)
+def save_config(folder_path=Path('/content'), model=None):    
+    _handle_not_exist_folder(folder_path)
     
     output_dict = {
         'date': str(mcfg.today,),
@@ -216,7 +341,7 @@ def save_config(folder_path='/content/', model=None):
         'other_settings': mcfg.description        
     }
     print(json.dumps(output_dict, indent=4))
-    target_path = os.path.join(folder_path, 'config.json')
+    target_path = folder_path / 'config.json'
     check = input(f'confirm saving {target_path}? (yes/y/n/no)')
     if check in ['y', 'yes']:
         print('start saving')
@@ -227,16 +352,12 @@ def save_config(folder_path='/content/', model=None):
         print('stop saving')
 
 def load_config(folder_path='.', file_path=None):
-    data = None
-    if file_path:
-        path = file_path
-    elif folder_path:
-        path = os.path.join(folder_path, 'config.json')
-    if not os.path.exists(path):
+    data = None    
+    path = file_path if file_path else folder_path / 'config.json'
+    print(f'config file path: {path}')
+    if not path.exists():
         print('This is a new model, still not config file')
     else:
         with open(path, 'r') as in_file:
             data = json.load(in_file)
     return data
-
-def get_train_steps
