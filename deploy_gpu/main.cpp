99380e19f1f98a7b8ded109005fe5630d234db6c
a8356555 <a8356555@gmail.com>
2021-08-28 16:27:33 +0000

    modifying deploy gpu

--- deploy_gpu/main.cpp
+++ deploy_gpu/main.cpp
@@ -1,12 +1,13 @@
-
+#include <string>
+#include "trt_inference.h"
 
 int main(int argc, char* argv[])
 {
     int n;
     if (argc<3)
     {
-        std::cerr << "usage " << argv[0] << " engine.trt image.jpg\n";
-        int n=100;
+        std::cerr << "basic usage: " << argv[0] << " engine.trt image.jpg\n" << "test speed usage: " << argv[0] << " engine.trt image.jpg " << std::endl;
+        
         return -1;
     }
     n = static_cast<int>(argv[3]);
