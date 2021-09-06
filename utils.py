024bc221b3717058f17e0a071f202a6def74c287
a8356555 <a8356555@gmail.com>
2021-09-06 09:38:59 +0000

    ...

--- utils.py
+++ utils.py
@@ -13,7 +13,7 @@ import torch
 from tensorboard.backend.event_processing import event_accumulator
 
 # please see #TODO
-ROOT = "/content/gdrive/MyDrive/SideProject/Yushan_AI_Competition"
+ROOT = "/content/gdrive/MyDrive/SideProject/YushanAICompetition"
 
 
 class ImageReader:
